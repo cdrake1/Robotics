20c469f405e88aed3b8f62bc0062be52659b0daf
RyanRosenkranse <97976510+RyanRosenkranse@users.noreply.github.com>
2023-02-20 18:34:59 -0500

    Add files via upload
    This is the version submitted for Lab2B.

---
+++ Lab2Bsubmitversion.ino
@@ -0,0 +1,266 @@
+//This program allows for the Pololu3piPlus to follow
+//an object using an Ultrasonic Sensor.
+//By Ryan Rosenkranse, Nick Suchy, Collin Drake
+//Version: Lab2B
+
+#include <Pololu3piPlus32U4.h>
+#include <Servo.h>
+
+using namespace Pololu3piPlus32U4;
+
+Encoders encoders;
+Buzzer buzzer;
+Motors motors;
+ButtonA buttonA;
+ButtonB buttonB;
+ButtonC buttonC;
+Servo headServo;
+
+// switches
+const boolean HEAD_DEBUG = false;
+const boolean TIMING_DEBUG = false;
+const boolean US_DEBUG = false;
+const boolean MOTOR_DEBUG = false;
+
+const boolean SERVO_ON = false;
+const boolean US_ON = true;
+
+// Head Servo Timing
+unsigned long headCm;
+unsigned long headPm;
+const unsigned long HEAD_MOVEMENT_PERIOD = 250;
+
+// Head servo constants
+const int HEAD_SERVO_PIN = 20;
+const int NUM_HEAD_POSITIONS = 3;
+const int HEAD_POSITIONS[NUM_HEAD_POSITIONS] = {105, 90, 75};
+
+// head servo data
+boolean headDirectionClockwise = true;
+int currentHeadPosition = 0;
+
+// Ultra Sonic Sensor constants
+const int ECHO_PIN = 12;
+const int TRIG_PIN = 18;
+
+const float MAX_DISTANCE = 30.0;
+
+const float DISTANCE_FACTOR = MAX_DISTANCE / 100;
+const float STOP_DISTANCE = 5;
+
+// Motor constants
+const float MOTOR_BASE_SPEED = 150.0;
+const int MOTOR_MIN_SPEED = 30;
+// determine the normalization factor based on MOTOR_BASE_SPEED
+const float MOTOR_FACTOR =  MOTOR_BASE_SPEED / 100;
+
+// motor compensation
+const float L_MOTOR_FACTOR = 1.0;
+const float R_MOTOR_FACTOR = 1.0;
+const float L_MOTOR_FACTOR_THRESHOLD = 80;
+const float R_MOTOR_FACTOR_THRESHOLD = 80;
+
+//ultrasonic timing
+unsigned long usCm;
+unsigned long usPm;
+const unsigned long US_PERIOD = 50;
+const unsigned long WAIT_AFTER_HEAD_STARTS_MOVING = 80;
+boolean usReadFlag = false;
+
+// current US distance reading
+int currentReadPosition = 0;
+
+float distanceReadings[NUM_HEAD_POSITIONS];
+
+// Motor Timing
+unsigned long motorCm;
+unsigned long motorPm;
+const unsigned long MOTOR_PERIOD = 20;
+
+float distance = 0;
+
+long countsLeft = 0;
+long countsRight = 0;
+long prevLeft = 0;
+long prevRight = 0;
+
+float Sl = 0.0F;
+float Sr = 0.0F;
+
+//DIAMETER AND CIRCUMFERENCE ARE IN CENTIMETERS
+const int CLICKS_PER_ROTATION = 12;
+const float GEAR_RATIO = 74.55F; //Slightly tweaked for accuracy
+const float WHEEL_DIAMETER = 3.2;
+const float WHEEL_CIRCUMFERENCE = 10.0531;
+float dist = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  Serial.begin(57600);
+
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+
+  //headServo.attach(HEAD_SERVO_PIN);
+  //headServo.write(70);
+
+  /*
+  for(int i=0; i<NUM_HEAD_POSITIONS; i++){
+    distanceReadings[i] = MAX_DISTANCE;
+  }
+  */
+
+  // start delay
+  delay(3000);
+  buzzer.play("c32");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  usReadCm();
+
+  //moveHead();
+
+  setMotors();
+
+}
+
+void moveHead() {
+  headCm = millis();
+  if(headCm > headPm + HEAD_MOVEMENT_PERIOD){
+
+    // head debug output
+    if(HEAD_DEBUG) {
+      Serial.print(currentHeadPosition);
+      Serial.print(" - ");
+      Serial.println(HEAD_POSITIONS[currentHeadPosition]);
+    }
+
+    // position head to the current position in the array
+    headServo.write( HEAD_POSITIONS[currentHeadPosition]);
+
+    // Set next head position
+    // Moves servo to the next head position and changes direction when needed
+    if(headDirectionClockwise){
+      if(currentHeadPosition >= (NUM_HEAD_POSITIONS -1)){
+        headDirectionClockwise = !headDirectionClockwise;
+        currentHeadPosition--;
+      }
+      else{
+        currentHeadPosition++;
+      }    
+    }else{
+      if(currentHeadPosition <=0){
+        headDirectionClockwise = !headDirectionClockwise;
+        currentHeadPosition++;
+      }else {
+        currentHeadPosition--;
+      }
+    }
+
+    // reset previous millis
+    headPm = headCm;
+    // reset read flag
+    usReadFlag =false;
+
+  }
+}
+
+void usReadCm() {
+  usCm = millis();
+  if(usCm > usPm + US_PERIOD) {
+
+    //clears the TRIG_PIN (set low)
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+
+    // Sets the TRIG_PIN HIGH (ACTIVE) for 10 microseconds
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    // Reads the ECHO_PIN, returns the sound wave travel time in microseconds
+    // note the duration (38000 microseconds) that will allow for reading up max distance supported by the sensor
+    float duration = pulseIn(ECHO_PIN, HIGH, 38000);
+
+    //caluclating the distance
+    distance = duration * 0.034 / 2; // Time of flight equation: Speed of sound
+    distance = floor(10 * distance + 0.5f) / 10;
+
+    //apply limits
+
+    if(distance > MAX_DISTANCE) distance = MAX_DISTANCE;
+    if (distance == 0) distance = MAX_DISTANCE;
+
+    // Displays the distance on the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+    // update the prevmillis
+    usPm = usCm;
+  }
+}
+
+void setMotors() {
+  motorCm = millis();
+  if(motorCm > motorPm + MOTOR_PERIOD) {
+  
+  // start out with the MOTOR_BASE_SPEED
+  float leftSpeed = MOTOR_BASE_SPEED;
+  float rightSpeed = MOTOR_BASE_SPEED;
+
+  // check to see if most current distance measurement is less than / euqal to MAX_DISTANCE
+  if(distance <= MAX_DISTANCE) {
+    // determine the magnitude of the distance by taking the difference
+    // divide by the DISTANCE_FACTOR to ensure uniform response as MAS_DISTANCE changes
+    // This maps the distance range (1 - MAX_RANGE) to 0-100 for the magnitude
+    float magnitude = (float)(MAX_DISTANCE - distance) / DISTANCE_FACTOR;
+    
+    leftSpeed = MOTOR_BASE_SPEED - (magnitude * MOTOR_FACTOR);
+    rightSpeed = MOTOR_BASE_SPEED - (magnitude * MOTOR_FACTOR);
+
+    if(leftSpeed < MOTOR_MIN_SPEED) leftSpeed = MOTOR_MIN_SPEED;
+    if(rightSpeed < MOTOR_MIN_SPEED) rightSpeed = MOTOR_MIN_SPEED;
+
+    // add in motor compensation
+    if(leftSpeed <= L_MOTOR_FACTOR_THRESHOLD) {
+        leftSpeed *= L_MOTOR_FACTOR;
+    }
+    if(rightSpeed <= R_MOTOR_FACTOR_THRESHOLD) {
+      rightSpeed *= R_MOTOR_FACTOR;
+    }
+
+
+    // Dynamics Contol Tuning
+    // if distance of object is between 7 and 5 then the robot begins to deaccelerate
+    // makes sure that speed does not go lower than 30.
+    if(distance <= 7 && distance >= 5){
+      leftSpeed= (10 * (abs((distance-leftSpeed)) / 10));
+      rightSpeed= (10 * (abs((distance-rightSpeed)) / 10));
+      if(leftSpeed < 30) leftSpeed = 30;
+      if(rightSpeed < 30) rightSpeed = 30;
+    }
+
+    if(distance <= STOP_DISTANCE){
+      leftSpeed = 0;    
+      rightSpeed = 0;
+    }
+
+
+    if(MOTOR_DEBUG){
+    Serial.print("Left: ");
+    Serial.println(leftSpeed);
+    Serial.print("Right: ");
+    Serial.println(rightSpeed);
+    }
+
+    motors.setSpeeds(-leftSpeed, -rightSpeed);
+
+    motorPm = motorCm;
+  }
+  }
+}
